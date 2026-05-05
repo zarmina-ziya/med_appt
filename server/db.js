20b0b26635e52b6d5f78d6d200712c5ee2ffe512
zarminaziya <zarminaziya@gmail.com>
2026-05-05 18:51:41 -0400

    server db added

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:2OGLVqRimIz7oSgpEh37WBEz@172.21.47.124:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
